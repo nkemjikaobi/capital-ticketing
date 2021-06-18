cef2ea7126672bb9f3f4125ad91c8b93583c2fc9
Obi NkemjIka Derick <obinkemjikaderick@Administrators-MacBook-Pro-7.local>
2021-06-18 14:13:28 +0100

    BasketBall Tickets and controllers

--- resources/views/dashboard/basketball/buy_tickets_basketball.blade.php
+++ resources/views/dashboard/basketball/buy_tickets_basketball.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <title>View Tickets</title>
+    <title>Buy Tickets BasketBall</title>
 
     <!-- GOOGLE FONTS -->
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500|Poppins:400,500,600,700|Roboto:400,500" rel="stylesheet"/>
@@ -224,143 +224,52 @@
                     @if (!empty($success))
                         {{ $success }}
                     @endif
-                    <div class='alert alert-primary '>
-                        <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
-                        <strong class=''>Once you sell a ticket, the current ROI is what gets credited in your account.</strong>
-                    </div>
 
                     <nav aria-label="breadcrumb">
                         <ol class="breadcrumb breadcrumb-inverse">
                             <li class="breadcrumb-item">
                                 <a href="#">Home</a>
                             </li>
-                            <li class="breadcrumb-item active" aria-current="page">View Tickets</li>
+                            <li class="breadcrumb-item" aria-current="page"><a href="/buy_tickets" style="text-decoration: none !important;color:#4c84ff;">Buy Tickets</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">BasketBall</li>
                         </ol>
                     </nav>
 
                 </div>
                 <!-- Top Statistics -->
-                <div class="row">
-                    <div class="col-lg-12">
-
-                        <!--TABLE STARTS -->
-                        <div class='card-body'>
-                            <div class='table-responsive'>
-                            @if(count($soccer_tickets) > 0 && count($basketball_tickets))
-                                <!-- Table -->
-                                <table id='dataTable' class='table table-bordered table-hover' width='100%' cellspacing='0'>
-                                    <thead>
-                                    <tr>
-                                        <th>Fixture</th>
-                                        <th>Country</th>
-                                        <th>Ticket Price</th>
-                                        <th>Number Bought</th>
-                                        <th>Amount Paid</th>
-                                        <th>Fixture Time</th>
-                                        <th>Fixture Date</th>
-                                        <th>Expected Profit</th>
-                                        <th>ROI</th>
-                                        <th>STATUS</th>
-                                        <th>ACTION</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-
-                                    </tbody>
-                                    <style>
-                                        td{
-                                            width: auto;
-                                        }
-                                        td:last-child{
-                                            width: 250px;
-                                        }
-                                    </style>
-
-                                    @foreach($soccer_tickets as $soccer_ticket)
-                                            <tr>
-                                                <td>{{$soccer_ticket->home_team}} - {{$soccer_ticket->away_team}}</td>
-                                                <td>{{$soccer_ticket->country}}</td>
-                                                <td>${{$soccer_ticket->ticket_price}}</td>
-                                                <td>{{$soccer_ticket->purchase_number}}</td>
-                                                <td>{{$soccer_ticket->final_pay}}</td>
-                                                <td>{{$soccer_ticket->fixture_time}}</td>
-                                                <td>{{$soccer_ticket->fixture_date}}</td>
-                                                <td>{{$soccer_ticket->expected_profit}}%</td>
-                                                <td>{{$soccer_ticket->roi}}</td>
-                                                <td>
-                                                    @if($soccer_ticket->transaction_status == 1)
-                                                        <button style='color: white;background:green;border-radius:5px;padding:10px;' id='status'>ACTIVE</button>
-                                                    @else
-                                                        <button style='color: white;background:red;border-radius:5px;padding:10px;' id='status'>INACTIVE</button>
-                                                    @endif
-                                                </td>
-                                                <td>
-                                                    @if($soccer_ticket->isSold == 1)
-                                                        <button href="#!" style='color: white;background:green;border-radius:5px;padding:10px;' id='sell'>SOLD</button>
-                                                    @else
-                                                        @if($soccer_ticket->transaction_status == 1)
-                                                            <form method="POST" action="/sell_tickets">
-                                                                @csrf
-                                                                <input type="hidden" value={{$soccer_ticket->id}} name="id"/>
-                                                                <input type="hidden" value={{$soccer_ticket->roi}} name="roi"/>
-                                                                <input type="hidden" value={{$soccer_ticket->tickets_available}} name="tickets_available"/>
-                                                                <button type="submit" style='color: white;background:#003699;border-radius:5px;padding:10px;' id='sell'>SELL</button>
-                                                            </form>
-
-                                                        @else
-                                                            <button href="#!" style='color: white;background:red;border-radius:5px;padding:10px;' id='sell'>CAN'T SELL</button>
-                                                        @endif
-                                                    @endif
-                                                </td>
-                                            </tr>
-                                        @endforeach
-                                        @foreach($basketball_tickets as $basketball_ticket)
-                                        <tr>
-                                            <td>{{$basketball_ticket->home_team}} - {{$basketball_ticket->away_team}}</td>
-                                            <td>{{$basketball_ticket->country}}</td>
-                                            <td>${{$basketball_ticket->ticket_price}}</td>
-                                            <td>{{$basketball_ticket->purchase_number}}</td>
-                                            <td>{{$basketball_ticket->final_pay}}</td>
-                                            <td>{{$basketball_ticket->fixture_time}}</td>
-                                            <td>{{$basketball_ticket->fixture_date}}</td>
-                                            <td>{{$basketball_ticket->expected_profit}}%</td>
-                                            <td>{{$basketball_ticket->roi}}</td>
-                                            <td>
-                                                @if($basketball_ticket->transaction_status == 1)
-                                                    <button style='color: white;background:green;border-radius:5px;padding:10px;' id='status'>ACTIVE</button>
-                                                @else
-                                                    <button style='color: white;background:red;border-radius:5px;padding:10px;' id='status'>INACTIVE</button>
-                                                @endif
-                                            </td>
-                                            <td>
-                                                @if($basketball_ticket->isSold == 1)
-                                                    <button href="#!" style='color: white;background:green;border-radius:5px;padding:10px;' id='sell'>SOLD</button>
-                                                @else
-                                                    @if($basketball_ticket->transaction_status == 1)
-                                                        <form method="POST" action="/sell_tickets">
-                                                            @csrf
-                                                            <input type="hidden" value={{$basketball_ticket->id}} name="id"/>
-                                                            <input type="hidden" value={{$basketball_ticket->roi}} name="roi"/>
-                                                            <input type="hidden" value={{$basketball_ticket->tickets_available}} name="tickets_available"/>
-                                                            <button type="submit" style='color: white;background:#003699;border-radius:5px;padding:10px;' id='sell'>SELL</button>
-                                                        </form>
-
-                                                    @else
-                                                        <button href="#!" style='color: white;background:red;border-radius:5px;padding:10px;' id='sell'>CAN'T SELL</button>
-                                                    @endif
-                                                @endif
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                </table>
-                                @else
-                                    <p>You do not have any active ticket...</p>
-                                @endif
+                <div class='row'>
+                    @foreach($tickets as $ticket)
+                            <div class="col-md-6">
+                                <div class="card text-center">
+                                    <div class="card-header">
+                                        {{ucwords($ticket->country)}} : {{ucwords($ticket->competition)}}
+                                    </div>
+                                    <div class="card-body">
+                                        <h5 class="card-title">
+                                            <img
+                                                src="{{ asset('/images/arsenal-logo.png') }}"
+                                                height="30px"
+                                                width="30px"
+                                                alt="">
+                                         {{$ticket->home_team }} - {{$ticket->away_team}}
+                                            <img
+                                                src="{{asset('/images/chelsea-logo.png')}}"
+                                                height="30px"
+                                                width="30px"
+                                                alt="">
+                                        </h5>
+                                        <p class="card-text">{{$ticket->fixture_date}} | {{$ticket->fixture_time}} GMT</p>
+                                        <h3 class="card-text">${{$ticket->ticket_price}}</h3>
+                                        <p class="card-text">Expected Profit: {{$ticket->expected_profit}}%</p>
+                                        <p class="card-text">Tickets Available: {{$ticket->tickets_available}}</p>
+                                        <a href="/buy_tickets/basketball/{{$ticket->id}}/{{$ticket->home_team}}_{{$ticket->away_team}}" class="btn btn-primary">BUY NOW</a>
+                                    </div>
+                                    <div class="card-footer text-muted">
+                                        {{$ticket->time_left}} days left to expire
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        <!--TABLE ENDS-->
-
-                    </div>
+                    @endforeach
                 </div>
             </div>
         </div>
@@ -377,7 +286,6 @@
                 document.getElementById("copy-year").innerHTML = year;
             </script>
         </footer>
-
     </div>
 </div>
 
@@ -403,6 +311,8 @@
 <script src="{{ asset('dashboard/assets/js/map.js') }}"></script>
 <script src="{{ asset('dashboard/assets/js/custom.js') }}"></script>
 
-
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,13 @@ Route::put('/profile', [App\Http\Controllers\HomeController::class, 'update_prof
 Route::get('/fund_wallet', [App\Http\Controllers\HomeController::class, 'fund_wallet'])->name('fund_wallet');
 Route::post('/fund_wallet/create', [App\Http\Controllers\HomeController::class, 'fund_wallet_create'])->name('fund_wallet_create');
 Route::get('/buy_tickets', [App\Http\Controllers\HomeController::class, 'buy_tickets'])->name('buy_tickets');
+Route::get('/buy_tickets/football', [App\Http\Controllers\HomeController::class, 'buy_tickets_football'])->name('buy_tickets_football');
+Route::get('/buy_tickets/cricket', [App\Http\Controllers\HomeController::class, 'buy_tickets_cricket'])->name('buy_tickets_cricket');
+Route::post('/sell_tickets', [App\Http\Controllers\HomeController::class, 'sell_tickets'])->name('sell_tickets');
+Route::get('/deposits', [App\Http\Controllers\HomeController::class, 'deposits'])->name('deposits');
+Route::get('/view_tickets', [App\Http\Controllers\HomeController::class, 'view_tickets'])->name('view_tickets');
+Route::get('/withdrawals', [App\Http\Controllers\HomeController::class, 'withdrawals'])->name('withdrawals');
+Route::post('/webhook', [App\Http\Controllers\CoinBaseController::class, 'webhook'])->name('webhook');
 
 //Soccer Routes
 Route::get('/buy_tickets/soccer', [App\Http\Controllers\SoccerController::class, 'buy_tickets_soccer'])->name('buy_tickets_soccer');
@@ -37,10 +44,7 @@ Route::get('/calculate_soccer_roi', [App\Http\Controllers\SoccerController::clas
 
 //BasketBall Routes
 Route::get('/buy_tickets/basketball', [App\Http\Controllers\BasketBallController::class, 'buy_tickets_basketball'])->name('buy_tickets_basketball');
-Route::get('/buy_tickets/football', [App\Http\Controllers\HomeController::class, 'buy_tickets_football'])->name('buy_tickets_football');
-Route::get('/buy_tickets/cricket', [App\Http\Controllers\HomeController::class, 'buy_tickets_cricket'])->name('buy_tickets_cricket');
-Route::post('/sell_tickets', [App\Http\Controllers\HomeController::class, 'sell_tickets'])->name('sell_tickets');
-Route::get('/deposits', [App\Http\Controllers\HomeController::class, 'deposits'])->name('deposits');
-Route::get('/view_tickets', [App\Http\Controllers\HomeController::class, 'view_tickets'])->name('view_tickets');
-Route::get('/withdrawals', [App\Http\Controllers\HomeController::class, 'withdrawals'])->name('withdrawals');
-Route::post('/webhook', [App\Http\Controllers\CoinBaseController::class, 'webhook'])->name('webhook');
+Route::get('/buy_tickets/basketball/{id}/{fixture}', [App\Http\Controllers\BasketballController::class, 'buy_tickets_basketball_fixture'])->name('buy_tickets_basketball_fixture');
+Route::post('/buy_tickets/basketball/pay', [App\Http\Controllers\BasketballController::class, 'buy_tickets_basketball_pay'])->name('buy_tickets_basketball_pay');
+Route::post('/buy_tickets/basketball/pay/process', [App\Http\Controllers\BasketballController::class, 'buy_tickets_basketball_pay_process'])->name('buy_tickets_basketball_pay_process');
+Route::get('/calculate_basketball_roi', [App\Http\Controllers\BasketballController::class, 'calculate_basketball_roi'])->name('calculate_basketball_roi');
