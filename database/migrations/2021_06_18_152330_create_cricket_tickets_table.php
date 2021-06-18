7efe20192a0389815c7c18b4f64011d2223f4a73
Obi NkemjIka Derick <obinkemjikaderick@Administrators-MacBook-Pro-7.local>
2021-06-18 16:33:59 +0100

    Routes, Controllers, Models, Factories for FootBall & Cricket

---
+++ database/migrations/2021_06_18_152330_create_cricket_tickets_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCricketTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cricket_tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('home_team');
+            $table->string('away_team');
+            $table->string("country");
+            $table->date("fixture_date");
+            $table->time("fixture_time");
+            $table->string("competition");
+            $table->decimal("ticket_price","10","2");
+            $table->integer("expected_profit");
+            $table->integer("tickets_available");
+            $table->integer("time_left");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cricket_tickets');
+    }
+}
