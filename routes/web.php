ccd760f03957bf1b7fb3227e04d8a019fbb29ad9
Obi NkemjIka Derick <obinkemjikaderick@Administrators-MacBook-Pro-7.local>
2021-06-18 10:34:26 +0100

    Moved Soocer Logic to a seperate controller

--- routes/web.php
+++ routes/web.php
@@ -27,14 +27,18 @@ Route::put('/profile', [App\Http\Controllers\HomeController::class, 'update_prof
 Route::get('/fund_wallet', [App\Http\Controllers\HomeController::class, 'fund_wallet'])->name('fund_wallet');
 Route::post('/fund_wallet/create', [App\Http\Controllers\HomeController::class, 'fund_wallet_create'])->name('fund_wallet_create');
 Route::get('/buy_tickets', [App\Http\Controllers\HomeController::class, 'buy_tickets'])->name('buy_tickets');
-Route::get('/buy_tickets/soccer', [App\Http\Controllers\HomeController::class, 'buy_tickets_soccer'])->name('buy_tickets_soccer');
-Route::get('/buy_tickets/soccer/{id}/{fixture}', [App\Http\Controllers\HomeController::class, 'buy_tickets_soccer_fixture'])->name('buy_tickets_soccer_fixture');
-Route::post('/buy_tickets/soccer/pay', [App\Http\Controllers\HomeController::class, 'buy_tickets_soccer_pay'])->name('buy_tickets_soccer_pay');
-Route::post('/buy_tickets/soccer/pay/process', [App\Http\Controllers\HomeController::class, 'buy_tickets_soccer_pay_process'])->name('buy_tickets_soccer_pay_process');
-Route::get('/buy_tickets/basketball', [App\Http\Controllers\HomeController::class, 'buy_tickets_basketball'])->name('buy_tickets_basketball');
+
+//Soccer Routes
+Route::get('/buy_tickets/soccer', [App\Http\Controllers\SoccerController::class, 'buy_tickets_soccer'])->name('buy_tickets_soccer');
+Route::get('/buy_tickets/soccer/{id}/{fixture}', [App\Http\Controllers\SoccerController::class, 'buy_tickets_soccer_fixture'])->name('buy_tickets_soccer_fixture');
+Route::post('/buy_tickets/soccer/pay', [App\Http\Controllers\SoccerController::class, 'buy_tickets_soccer_pay'])->name('buy_tickets_soccer_pay');
+Route::post('/buy_tickets/soccer/pay/process', [App\Http\Controllers\SoccerController::class, 'buy_tickets_soccer_pay_process'])->name('buy_tickets_soccer_pay_process');
+Route::get('/calculate_soccer_roi', [App\Http\Controllers\SoccerController::class, 'calculate_soccer_roi'])->name('calculate_soccer_roi');
+
+//BasketBall Routes
+Route::get('/buy_tickets/basketball', [App\Http\Controllers\BasketBallController::class, 'buy_tickets_basketball'])->name('buy_tickets_basketball');
 Route::get('/buy_tickets/football', [App\Http\Controllers\HomeController::class, 'buy_tickets_football'])->name('buy_tickets_football');
 Route::get('/buy_tickets/cricket', [App\Http\Controllers\HomeController::class, 'buy_tickets_cricket'])->name('buy_tickets_cricket');
-Route::get('/calculate_roi', [App\Http\Controllers\HomeController::class, 'calculate_roi'])->name('calculate_roi');
 Route::post('/sell_tickets', [App\Http\Controllers\HomeController::class, 'sell_tickets'])->name('sell_tickets');
 Route::get('/deposits', [App\Http\Controllers\HomeController::class, 'deposits'])->name('deposits');
 Route::get('/view_tickets', [App\Http\Controllers\HomeController::class, 'view_tickets'])->name('view_tickets');
